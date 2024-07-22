8cb9773af9688edb473df9b2ac4877f8e1244554
Aditya <ricky.pathak902@gmail.com>
2024-07-22 23:35:46 +0530

    making UserModel

---
+++ src/model/user.ts
@@ -0,0 +1,80 @@
+import mongoose, { Schema, Document } from "mongoose";
+
+//this provide the type safety for the schema that we gonna create in mongoose
+export interface Message extends Document {
+  content: string;
+  createdAt: Date;
+}
+
+//in typeScript the string is denoted as "string" and in mongoose it will denoted as "String"
+
+//this is the Schema for mongoose
+const MessageSchema: Schema<Message> = new Schema({
+  content: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    required: true,
+    default: Date.now,
+  },
+});
+
+//userSchema types in typeScript
+export interface User extends Document {
+  username: string;
+  email: string;
+  password: string;
+  verifyCode: string;
+  verifyCodeExpiry: Date;
+  isVerified: boolean;
+  isAcceptingMessage: boolean;
+  messages: Message[];
+}
+
+//userSchema in mongoose
+
+const UserSchema: Schema<User> = new Schema({
+  username: {
+    type: String,
+    required: [true, "Username is required"],
+    trim: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: [true, "Email is required"],
+    unique: true,
+    match: [/.+\@.+\..+/, "please use a valid email address"],
+  },
+  password: {
+    type: String,
+    required: [true, "Password is required"],
+  },
+  verifyCode: {
+    type: String,
+    required: [true, "Verify is required"],
+  },
+  verifyCodeExpiry: {
+    type: Date,
+    required: [true, "Verify code expiry is required"],
+  },
+  isVerified: {
+    type: Boolean,
+    default: false,
+  },
+  isAcceptingMessage: {
+    type: Boolean,
+    default: true,
+  },
+  messages: [MessageSchema],
+});
+
+//how to export this schema
+const UserModel =
+  (mongoose.models.User as mongoose.Model<User>) ||
+  mongoose.model<User>("User", UserSchema);
+export default UserModel;
+
+//"as mongoose.Model<User>" this code dedicate the type of the schema or typescript
